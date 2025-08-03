2f8e7ec94cebbbf7f36d5dd580eff7e10be6ef14
shivam <108524973+shivamness@users.noreply.github.com>
2025-08-03 20:08:59 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
         return [
             {
                 source: '/resume',
-                destination: 'https://drive.google.com/file/d/1aq-f3ucT34IzP04o1fs_xwTwNCzvlDzc/view',
+                destination: 'https://drive.google.com/file/d/1J3AJ3OSwHYdAPlDQQrQ23TPDXIU1E6HV/view',
                 permanent: false,
             }
            ] 
